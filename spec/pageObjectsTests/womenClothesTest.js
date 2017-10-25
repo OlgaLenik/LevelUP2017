f440b16882a3d954241c903524cadb1e879201a0
CORP\OMASLANI <olga.maslanik@capgemini.com>
2017-10-25 21:05:41 +0200

    update

--- spec/pageObjectsTests/womenClothesTest.js
+++ spec/pageObjectsTests/womenClothesTest.js
@@ -1,99 +1,99 @@
-var HomePage = require(pageObjectDir + "/homePage.js");
+// var HomePage = require(pageObjectDir + "/homePage.js");
 
-var homePage = new HomePage();
+// var homePage = new HomePage();
 
-var WomenPage = require(pageObjectDir + "/womenPage.js");
+// var WomenPage = require(pageObjectDir + "/womenPage.js");
 
-var womenPage = new WomenPage();
+// var womenPage = new WomenPage();
 
-var TopsPage = require(pageObjectDir + "/topsPage.js");
+// var TopsPage = require(pageObjectDir + "/topsPage.js");
 
-var topsPage = new TopsPage();
+// var topsPage = new TopsPage();
 
-var FadedShortSleevePage = require(pageObjectDir + "/fadedShortSleevePage.js");
+// var FadedShortSleevePage = require(pageObjectDir + "/fadedShortSleevePage.js");
 
-var fadedShortSleevePage = new FadedShortSleevePage();
+// var fadedShortSleevePage = new FadedShortSleevePage();
 
-var AfterBuyPage = require(pageObjectDir + "/afterBuyPage.js");
+// var AfterBuyPage = require(pageObjectDir + "/afterBuyPage.js");
 
-var afterBuyPage = new AfterBuyPage();
+// var afterBuyPage = new AfterBuyPage();
 
-var BasketPage = require(pageObjectDir + "/basketPage.js");
+// var BasketPage = require(pageObjectDir + "/basketPage.js");
 
-var basketPage = new BasketPage();
+// var basketPage = new BasketPage();
 
-var blouseName;
+// var blouseName;
 
-var price;
+// var price;
 
-describe('check womenClothesPage', function () {
-    it('check if homePage loaded', function () {
-        browser.get(homePage.URL);
-        expect(browser.getTitle())
-            .toEqual("My Store");
-    });
+// describe('check womenClothesPage', function () {
+//     it('check if homePage loaded', function () {
+//         browser.get(homePage.URL);
+//         expect(browser.getTitle())
+//             .toEqual("My Store");
+//     });
 
-    it('open womenPage', function () {
-        homePage.womenButton.click();
-        expect(browser.getTitle())
-            .toEqual("Women - My Store");
-    });
+//     it('open womenPage', function () {
+//         homePage.womenButton.click();
+//         expect(browser.getTitle())
+//             .toEqual("Women - My Store");
+//     });
 
-    it('open topsPage', function () {
-        womenPage.topsPageButton.click();
-        expect(browser.getTitle())
-            .toEqual("Tops - My Store");
-    });
+//     it('open topsPage', function () {
+//         womenPage.topsPageButton.click();
+//         expect(browser.getTitle())
+//             .toEqual("Tops - My Store");
+//     });
 
-    it('open ded Short Sleeve Page', function () {
-        topsPage.fadedShortSleeveButton.click();
-        var isLabelLongerThan = function (isLongerThan) {
-            return new Promise(function (resolve) {
-                fadedShortSleevePage.fadedShortSleeveTitle.getText().then(function (text) {
-                    expect(text.length).toBeGreaterThan(isLongerThan);
-                })
-            });
-        }
-        isLabelLongerThan(8).then(console.log)
-        blouseName = fadedShortSleevePage.fadedShortSleeveTitle.getText();
-        price = fadedShortSleevePage.fadedShortSleevePrise.getText();
-    });
+//     it('open ded Short Sleeve Page', function () {
+//         topsPage.fadedShortSleeveButton.click();
+//         var isLabelLongerThan = function (isLongerThan) {
+//             return new Promise(function (resolve) {
+//                 fadedShortSleevePage.fadedShortSleeveTitle.getText().then(function (text) {
+//                     expect(text.length).toBeGreaterThan(isLongerThan);
+//                 })
+//             });
+//         }
+//         isLabelLongerThan(8).then(console.log)
+//         blouseName = fadedShortSleevePage.fadedShortSleeveTitle.getText();
+//         price = fadedShortSleevePage.fadedShortSleevePrise.getText();
+//     });
 
-    it('open topsPage', function () {
-        expect(fadedShortSleevePage.fadedShortSleeveConditionLabel.getText())
-            .toEqual("New");
-    });
+//     it('open topsPage', function () {
+//         expect(fadedShortSleevePage.fadedShortSleeveConditionLabel.getText())
+//             .toEqual("New");
+//     });
 
-    it('it choose size and clikck submitButton', function () {
-        fadedShortSleevePage.sizeDropDown.click();
-        expect(fadedShortSleevePage.submitButton.getText()).toEqual("Add to cart");
-        fadedShortSleevePage.submitButton.click();
-    });
+//     it('it choose size and clikck submitButton', function () {
+//         fadedShortSleevePage.sizeDropDown.click();
+//         expect(fadedShortSleevePage.submitButton.getText()).toEqual("Add to cart");
+//         fadedShortSleevePage.submitButton.click();
+//     });
 
 
-    it('load homePage', function () {
-        browser.get(homePage.URL);
-        expect(browser.getTitle())
-            .toEqual("My Store");
-    });
+//     it('load homePage', function () {
+//         browser.get(homePage.URL);
+//         expect(browser.getTitle())
+//             .toEqual("My Store");
+//     });
 
-    it('click cartButton', function () {
-        homePage.cartButton.click();
-        expect(browser.getTitle())
-            .toEqual("Order - My Store");
-    });
+//     it('click cartButton', function () {
+//         homePage.cartButton.click();
+//         expect(browser.getTitle())
+//             .toEqual("Order - My Store");
+//     });
 
-    it('check if product added to cart', function () {
-        basketPage.productLabel.isDisplayed();
-        expect(basketPage.productLabel.getText())
-            .toEqual(blouseName);
+//     it('check if product added to cart', function () {
+//         basketPage.productLabel.isDisplayed();
+//         expect(basketPage.productLabel.getText())
+//             .toEqual(blouseName);
 
-    });
+//     });
 
-    it('check total prise', function () {
-        basketPage.productPrise.isDisplayed();
-        expect(basketPage.productPrise.getText())
-            .toEqual(price);
+//     it('check total prise', function () {
+//         basketPage.productPrise.isDisplayed();
+//         expect(basketPage.productPrise.getText())
+//             .toEqual(price);
 
-    });
-});
+//     });
+// });
